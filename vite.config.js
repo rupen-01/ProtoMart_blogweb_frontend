8d30a434a1f50549e885d1cf96dda66df508a67f
pehliSite <91202215+priyanshmalakar@users.noreply.github.com>
2026-01-03 16:16:34 +0530

    update the encv and the backend link to the frontend

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://protomart-blogweb-backend-6xor.onrender.com',
         changeOrigin: true,
       }
     }
